8a522be0059a10e15756894189ee9bee67586089
Vincent Noël <contact@vincent-noel.fr>
2018-09-03 14:05:08 +0200

    Frontend urls now won't work for *everythin*, so will return 404 when the page doesn't exists

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,13 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-	# path('', views.index, name="home"),
-	re_path('.*', views.index, name="home"),
+
+	path('', views.index, name="home"),
+	path('login/', views.index, name="login"),
+	path('logout/', views.index, name="logout"),
+	path('profile/', views.index, name="profile"),
+	path('register/', views.index, name="register"),
+	path('models/', views.index, name="models"),
+	path('data/', views.index, name="data"),
+	path('model/.*', views.index, name="model"),
 ]
