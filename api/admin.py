1ce7796672214ce1e3194cea96af7f6907e1a983
Vincent Noël <contact@vincent-noel.fr>
2018-09-04 12:02:58 +0200

    Added project model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from api.models import LogicalModel, MaBoSSSimulation
+from api.models import Project, LogicalModel, MaBoSSSimulation
 # Register your models here.
 
+admin.site.register(Project)
 admin.site.register(LogicalModel)
 admin.site.register(MaBoSSSimulation)
--- api/models.py
+++ api/models.py
@@ -1,32 +1,72 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.contrib.auth.models import User
-from os import remove
-from os.path import join
+
+from random import choice
+from string import ascii_uppercase, ascii_lowercase, digits
+from os import remove, mkdir
+from os.path import join, isdir
+from shutil import rmtree
+
+def add_default_project(sender, instance, **kwargs):
+	if len(Project.objects.filter(user=instance)) == 0:
+		default_project = Project(user=instance, name="My Project")
+		default_project.save()
+
+def new_project_path():
+	rand_string = ''.join(choice(ascii_uppercase + ascii_lowercase + digits) for _ in range(12))
+	while isdir(join(settings.MEDIA_ROOT, rand_string)):
+		rand_string = ''.join(choice(ascii_uppercase + ascii_lowercase + digits) for _ in range(12))
+	mkdir(join(settings.MEDIA_ROOT, rand_string))
+	return rand_string
+
+
+def remove_project_path(sender, instance, **kwargs):
+	if isdir(join(settings.MEDIA_ROOT, instance.path)):
+		rmtree(join(settings.MEDIA_ROOT, instance.path))
+
 
 def path_logical_model(instance, filename):
+	return join(instance.user.project.path, 'logical_models', filename)
 
-	return join(instance.user.username, 'logical_models', filename)
 
 def remove_logical_model(sender, instance, **kwargs):
-
 	if instance.file:
 		remove(join(settings.MEDIA_ROOT, instance.file.path))
 
 
-# Create your models here.
-class LogicalModel(models.Model):
+def remove_simulation_model(sender, instance, **kwargs):
+	if instance.model_file:
+		remove(join(settings.MEDIA_ROOT, instance.model_file.path))
+
+
+
+class Project(models.Model):
 
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=256)
+	description = models.CharField(max_length=102400, blank=True)
+	path = models.CharField(max_length=256, default=new_project_path)
+
+
+pre_delete.connect(remove_project_path, sender=Project)
+post_save.connect(add_default_project, sender=User)
+
+class LogicalModel(models.Model):
+
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
+	name = models.CharField(max_length=256)
 	file = models.FileField(upload_to=path_logical_model)
+
 
 pre_delete.connect(remove_logical_model, sender=LogicalModel)
 
 
-
 class MaBoSSSimulation(models.Model):
+
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
+	model_file = models.FileField(upload_to=path_logical_model)
 
 	fptable = models.CharField(max_length=102400, null=True)
 	nodes_probtraj = models.CharField(max_length=102400, null=True)
@@ -49,3 +89,7 @@ class MaBoSSSimulation(models.Model):
 
 	status = models.CharField(max_length=15, choices=STATUSES, default=QUEUED)
 	error = models.CharField(max_length=255, default="", null=True)
+
+
+pre_delete.connect(remove_simulation_model, sender=MaBoSSSimulation)
+
