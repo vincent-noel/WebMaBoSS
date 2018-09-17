e21f3d027f148549a9aa5c2e8f67d88083604688
Vincent Noël <contact@vincent-noel.fr>
2018-09-17 12:40:59 +0200

    Loading icon can now handle css units

--- frontend/src/components/commons/LoadingIcon.js
+++ frontend/src/components/commons/LoadingIcon.js
@@ -3,7 +3,11 @@ import React from "react";
 class LoadingIcon extends React.Component {
 
 	render() {
-		return <div align="center"><img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" width={this.props.width} /></div>;
+		return <div align="center">
+			<img
+				src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+				style={{width: this.props.width}} />
+		</div>;
 	}
 }
 
