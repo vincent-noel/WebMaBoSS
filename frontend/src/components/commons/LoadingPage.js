5f2d2a7553443e043354698a7ca6867b7b0f4878
Vincent Noël <contact@vincent-noel.fr>
2018-11-08 17:55:23 +0100

    Added a Loading page component

---
+++ frontend/src/components/commons/LoadingPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './loading-page.scss';
+import '../../images/loading.svg';
+import '../../images/loading_dark.svg';
+
+class LoadingPage extends React.Component {
+
+	render() {
+
+		const src = this.props.dark !== undefined ? "/static/images/loading_dark.svg" : "/static/images/loading.svg";
+		return <div class="loading-page">
+			<img
+				src={src}
+			/>
+		</div>;
+	}
+}
+
+export default LoadingPage;
