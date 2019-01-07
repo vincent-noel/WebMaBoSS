d83c86c432fe8e71d5c6bd5d2a1ef5a5ec48fa71
Vincent Noël <contact@vincent-noel.fr>
2019-01-07 17:32:44 +0100

    Using create_default_db script in prod dockerfile

--- create_default_db.sh
+++ create_default_db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sleep 5
-python manage.py makemigrations -v 2 --settings=settings.test_settings
-python manage.py migrate -v 2 --settings=settings.test_settings
-python manage.py collectstatic --noinput > /dev/null && \
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+python manage.py makemigrations api -v 2
+python manage.py migrate -v 2
+python manage.py collectstatic --noinput > /dev/null
