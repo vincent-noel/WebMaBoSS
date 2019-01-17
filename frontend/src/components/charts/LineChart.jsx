2c0054ae807d5f59676a97bcb04844596dafda49
Vincent Noël <contact@vincent-noel.fr>
2019-01-17 15:21:25 +0100

    Improved download button for piechart, added to linechart

--- frontend/src/components/charts/LineChart.jsx
+++ frontend/src/components/charts/LineChart.jsx
@@ -3,16 +3,30 @@ import ReactDOMServer from "react-dom/server";
 import {Line} from "react-chartjs-2";
 
 import "./chart-legend.scss"
+import html2canvas from "html2canvas";
+import {Button} from "reactstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSave} from "@fortawesome/free-solid-svg-icons";
+import LoadingIcon from "../commons/loaders/LoadingIcon";
 
 class LineChart extends React.Component {
 
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			imgHref: null
+		};
+
 		this.chartRef = this.chartRef.bind(this);
 		this.legendRef = this.legendRef.bind(this);
+		this.downloadRef = React.createRef();
 
-		this.chartInstance = undefined;
+		this.chartInstance = null;
+		this.legend = null;
+
+		this.chartWidth = null;
+		this.chartHeight = null;
 	}
 
 	chartRef(ref) {
@@ -23,6 +37,7 @@ class LineChart extends React.Component {
 
 	legendRef(ref) {
 		if (ref !== null) {
+			this.legend = ref;
 			ref.innerHTML = this.chartInstance.generateLegend();
 
 			Object.values(ref.childNodes).map((child, index) => {
@@ -43,6 +58,7 @@ class LineChart extends React.Component {
 	   		element.classList.add('disable-legend')
 	   	}
 
+	   	this.setState({imgHref: null});
 	   	this.chartInstance.update();
 	}
 
@@ -80,12 +96,78 @@ class LineChart extends React.Component {
 						</div>
 					)
 				),
+				title: {
+					display: true,
+					text: this.props.title
+				},
+
+				animation : {
+					onComplete : (animation) => {
+
+						// Here we have a problem, which is html2canvas triggers the animation, and is launch after the
+						// animation completes. So it's a loop. So what we did is to only print it once after loading,
+						// or after clicking on the legend, or after a detected size change
+						if (this.chartInstance.width > 0 && (this.state.imgHref == null || this.chartHeight !== this.chartInstance.canvas.height || this.chartWidth !== this.chartInstance.canvas.width)){
+
+							this.chartHeight = this.chartInstance.canvas.height;
+							this.chartWidth = this.chartInstance.canvas.width;
+
+							let canvas_res = document.createElement('canvas');
+
+							canvas_res.width = this.chartInstance.canvas.width;
+							canvas_res.height = (
+								this.chartInstance.canvas.height +
+								this.legend.offsetHeight
+							);
+
+							let ctx_res = canvas_res.getContext("2d");
+							ctx_res.drawImage(this.chartInstance.canvas, 0, 0);
+
+							html2canvas(this.legend, {
+								width: this.legend.offsetWidth,
+								windowWidth: this.legend.offsetWidth,
+                                backgroundColor: null,
+                                x: 0,
+								logging: false
+							}).then(canvas => {
+
+								ctx_res.drawImage(canvas, 0, this.chartInstance.canvas.height);
+
+								let myImage = canvas_res.toDataURL("image/png");
+								this.setState({imgHref: myImage});
+							});
+						}
+					}
+    			},
+
 			};
 
 			return (
 				<React.Fragment>
-					<Line data={data} options={options} ref={this.chartRef}/>
-					<div ref={this.legendRef} className={"chart-legend"}></div>
+					<div onMouseOver={() => {
+						if (this.downloadRef.current !== null && this.chartInstance !== null && this.legend !== null) {
+							this.downloadRef.current.style.display = "flex";
+							this.downloadRef.current.style.justifyContent = "end";
+							this.downloadRef.current.style.position = "relative";
+							this.downloadRef.current.style.top = (- this.chartInstance.canvas.offsetHeight - this.legend.offsetHeight + 4).toString() + "px";
+						}
+
+					}} onMouseOut={() => {
+						if (this.downloadRef.current !== null) {
+							this.downloadRef.current.style.display= "none";
+						}
+
+					}}>
+						<Line data={data} options={options} ref={this.chartRef}/>
+						<div ref={this.legendRef} className={"chart-legend"}></div>
+						{ this.state.imgHref !== null ?
+							<a href={this.state.imgHref} download="chart.png" ref={this.downloadRef} style={{display: "none"}}>
+								<Button className="ml-1">
+									<FontAwesomeIcon icon={faSave}/>
+								</Button>
+							</a> : null
+						}
+					</div>
 				</React.Fragment>
 			);
 		}
--- frontend/src/components/charts/PieChart.jsx
+++ frontend/src/components/charts/PieChart.jsx
@@ -4,6 +4,9 @@ import ReactDOMServer from "react-dom/server";
 import html2canvas from "html2canvas";
 import "./chart-legend.scss"
 import {Button} from "reactstrap";
+import {faSave} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import LoadingIcon from "./LineChart";
 
 class PieChart extends React.Component {
 
@@ -13,11 +16,17 @@ class PieChart extends React.Component {
 		this.state = {
 			imgHref: null
 		};
+
 		this.chartRef = this.chartRef.bind(this);
 		this.legendRef = this.legendRef.bind(this);
-		this.divRef = React.createRef();
+		this.downloadRef = React.createRef();
 
 		this.chartInstance = null;
+		this.legend = null;
+
+		this.chartWidth = null;
+		this.chartHeight = null;
+
 	}
 
 	chartRef(ref) {
@@ -28,6 +37,7 @@ class PieChart extends React.Component {
 
 	legendRef(ref) {
 		if (ref !== null) {
+			this.legend = ref;
 			ref.innerHTML = this.chartInstance.generateLegend();
 
 			Object.values(ref.childNodes).map((child) => {
@@ -92,30 +102,74 @@ class PieChart extends React.Component {
 
 				animation : {
 					onComplete : (animation) => {
+
 						// Here we have a problem, which is html2canvas triggers the animation, and is launch after the
 						// animation completes. So it's a loop. So what we did is to only print it once after loading,
-						// or after clicking on the legend
-						if (this.state.imgHref == null){
-							html2canvas(this.divRef.current, {windowWidth: this.divRef.current.offsetWidth}).then(canvas => {
-								let myImage = canvas.toDataURL("image/png");
+						// or after clicking on the legend, or after a detected size change
+						if (this.chartInstance.width > 0 && (this.state.imgHref == null || this.chartHeight !== this.chartInstance.canvas.height || this.chartWidth !== this.chartInstance.canvas.width)){
+
+							this.chartHeight = this.chartInstance.canvas.height;
+							this.chartWidth = this.chartInstance.canvas.width;
+
+							let canvas_res = document.createElement('canvas');
+
+							canvas_res.width = this.chartInstance.canvas.width;
+							canvas_res.height = (
+								this.chartInstance.canvas.height +
+								this.legend.offsetHeight
+							);
+
+							let ctx_res = canvas_res.getContext("2d");
+							ctx_res.drawImage(this.chartInstance.canvas, 0, 0);
+
+							html2canvas(this.legend, {
+								width: this.legend.offsetWidth,
+								windowWidth: this.legend.offsetWidth,
+                                backgroundColor: null,
+                                x: 0,
+								logging: false
+							}).then(canvas => {
+
+								ctx_res.drawImage(canvas, 0, this.chartInstance.canvas.height);
+
+								let myImage = canvas_res.toDataURL("image/png");
 								this.setState({imgHref: myImage});
 							});
 						}
 					}
-    			}
+    			},
+
 			};
 
 			return <React.Fragment>
-				<div ref={this.divRef}>
+				<div
+					onMouseOver={() => {
+					if (this.downloadRef.current !== null && this.chartInstance !== null && this.legend !== null) {
+						this.downloadRef.current.style.display = "flex";
+						this.downloadRef.current.style.justifyContent = "end";
+						this.downloadRef.current.style.position = "relative";
+						this.downloadRef.current.style.top = (- this.chartInstance.canvas.offsetHeight - this.legend.offsetHeight + 4).toString() + "px";
+
+						}
+					}}
+
+					onMouseOut={() => {
+						if (this.downloadRef.current !== null) {
+							this.downloadRef.current.style.display= "none";
+						}
+					}}
+				>
 					<Pie data={data} options={options} ref={this.chartRef}/>
 					<div ref={this.legendRef} className={"chart-legend"}></div>
+					{ this.state.imgHref !== null ?
+						<a href={this.state.imgHref} download="chart.png" ref={this.downloadRef} style={{display: "none"}}>
+							<Button className="ml-1">
+								<FontAwesomeIcon icon={faSave}/>
+							</Button>
+						</a> : null
+					}
+
 				</div>
-				{ this.state.imgHref !== null ?
-					<a href={this.state.imgHref} download="chart.png">
-						<Button className="mr-1">Download</Button>
-					</a> :
-					null
-				}
 
 			</React.Fragment>;
 		}
