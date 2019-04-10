2e161f3ac96f967d8333cd5307085f38a05e6795
Vincent Noël <contact@vincent-noel.fr>
2019-04-10 14:41:27 +0200

    Fixed settings file path

--- create_default_db.sh
+++ create_default_db.sh
@@ -4,4 +4,4 @@ sleep 5
 python manage.py makemigrations api -v 2 "$@"
 python manage.py migrate -v 2 "$@"
 python manage.py collectstatic --noinput "$@" > /dev/null
-printf "secret_key: fwhfiewqhfiquwehfduihwq\nallowed_hosts: ['*']\n" > /appCurie/config.yml
+printf "secret_key: fwhfiewqhfiquwehfduihwq\nallowed_hosts: ['*']\n" > /appCurie/data/settings/config.yml
