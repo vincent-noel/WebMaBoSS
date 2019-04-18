fb9e68e3f29feec0ef176c415e1b43d586cf200b
Vincent Noël <contact@vincent-noel.fr>
2019-04-18 12:01:41 +0200

    Added frontend specific fixtures

--- settings/test_settings.py
+++ settings/test_settings.py
@@ -7,5 +7,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(TMP_ROOT, ''.join(choice(ascii_lowercase) for _ in range(10)))
 makedirs(MEDIA_ROOT, exist_ok=True)
 
-FIXTURE_DIRS = (os.path.join(BASE_DIR, 'api/tests/fixtures'),)
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'api/tests/fixtures'),
+    os.path.join(BASE_DIR, 'frontend/tests/fixtures')
+)
 
