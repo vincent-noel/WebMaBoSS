327892952382439ccb207c0fe304e241092dc60a
Vincent Noël <contact@vincent-noel.fr>
2019-08-14 14:18:13 +0200

    create_default_db.sh now checks if config exists before writing it

--- create_default_db.sh
+++ create_default_db.sh
@@ -3,4 +3,7 @@
 python manage.py makemigrations api -v 2 "$@"
 python manage.py migrate -v 2 "$@"
 python manage.py collectstatic --noinput "$@" > /dev/null
-printf "secret_key: fwhfiewqhfiquwehfduihwq\nallowed_hosts: ['*']\n" > /appCurie/data/settings/config.yml
+if [ ! -f /appCurie/data/settings/config.yml ]; then
+  random_string=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 24`
+  printf "secret_key: %s\nallowed_hosts: ['*']\n" "$random_string"> /appCurie/data/settings/config.yml
+fi
