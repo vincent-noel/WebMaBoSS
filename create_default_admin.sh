4fef73467b73b3b5b0c54019169a0740989e59f5
Vincent Noël <contact@vincent-noel.fr>
2019-08-15 15:41:37 +0200

    Change folder in docker

--- create_default_admin.sh
+++ create_default_admin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
-printf "admin: admin\nadmin_email: admin@example.com\n" >> /appCurie/data/settings/config.yml
+printf "admin: admin\nadmin_email: admin@example.com\n" >> /var/webmaboss/data/settings/config.yml
--- create_default_db.sh
+++ create_default_db.sh
@@ -3,7 +3,7 @@
 python manage.py makemigrations api -v 2 "$@"
 python manage.py migrate -v 2 "$@"
 python manage.py collectstatic --noinput "$@" > /dev/null
-if [ ! -f /appCurie/data/settings/config.yml ]; then
+if [ ! -f /var/webmaboss/data/settings/config.yml ]; then
   random_string=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 24`
-  printf "secret_key: %s\nallowed_hosts: ['*']\n" "$random_string"> /appCurie/data/settings/config.yml
+  printf "secret_key: %s\nallowed_hosts: ['*']\n" "$random_string"> /var/webmaboss/data/settings/config.yml
 fi
