0a465312e09e1f6dfe6d88e1fb4f6d284c899a74
Vincent Noël <contact@vincent-noel.fr>
2019-09-06 16:19:35 +0200

    Lowering status update frequency

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,5 +1,5 @@
 class Settings {
-    static updateRate = 2000;
+    static updateRate = 10000;
     static colormap = ['#4c72b0', '#55a868', '#c44e52', '#8172b2', '#ccb974', '#64b5cd', '#4c72b0', '#55a868', '#c44e52', '#8172b2'];
 }
 
