6ce72ebd35b1adfa3de4b07ec30a36d0ed5906a5
Vincent Noël <contact@vincent-noel.fr>
2020-06-18 12:14:16 +0200

    Calling python3 explicitely

--- create_default_db.sh
+++ create_default_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python manage.py makemigrations api -v 2 "$@"
-python manage.py migrate -v 2 "$@"
-python manage.py collectstatic --noinput "$@" > /dev/null
+python3 manage.py makemigrations api -v 2 "$@"
+python3 manage.py migrate -v 2 "$@"
+python3 manage.py collectstatic --noinput "$@" > /dev/null
 if [ ! -f /var/webmaboss/data/settings/config.yml ]; then
   random_string=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 24`
   printf "secret_key: %s\nallowed_hosts: ['*']\n" "$random_string"> /var/webmaboss/data/settings/config.yml
--- runserver.sh
+++ runserver.sh
@@ -4,5 +4,6 @@ if [ ! -d "./data" ]; then
     ./install.sh
 fi
 
+yarn && \
 npm run dev && \
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
