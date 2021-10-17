476fef3a4e6a55c693db73091840efec22abc061
Vincent Noël <contact@vincent-noel.fr>
2021-10-17 20:48:15 +0200

    Only checking to add guest account if a DB is configured

--- api/apps.py
+++ api/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class LogicalmodellingConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        from api.models.common import create_guest_account
-        create_guest_account()
+        
+        if settings.DATABASES['default']['HOST'] is not None:
+        
+            from api.models.common import create_guest_account
+            create_guest_account()
