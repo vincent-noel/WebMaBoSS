bc46acf0e36c69fdce75e4ac1f06d6b41c101cff
Vincent Noël <contact@vincent-noel.fr>
2021-10-19 10:35:03 +0200

    Fixed about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,13 +10,13 @@ class About extends React.Component {
 			It is open-source and available on <b><a href="https://github.com/sysbio-curie/WebMaBoSS">GitHub</a></b>, where you can also find instructions to run it locally and tutorials.
 			<br/><br/>
 			<h3>Publications</h3><br/>
-			
-			Noel V, Ruscone M, Stoll G, Viara E, Zinoviev A, Barillot E, Calzone L. WebMaBoSS: A web interface for simulating Boolean models stochastically. <i>In Preparation</i>.
-			<br/>
-			Stoll G, Viara E, Barillot E, Calzone L. Continuous time Boolean modeling for biological signaling: application of Gillespie algorithm. BMC Syst Biol. 2012 Aug 29;6:116. doi: <a href="https://doi.org/10.1186/1752-0509-6-116">10.1186/1752-0509-6-116</a>.
-		   	<br/>
-			Stoll G, Caron B, Viara E, Dugourd A, Zinovyev A, Naldi A, Kroemer G, Barillot E, Calzone L.  MaBoSS 2.0: an environment for stochastic Boolean modeling. Bioinformatics btx123. 2017 Mar. DOI: <a href="https://doi.org/10.1093/bioinformatics/btx123">10.1093/bioinformatics/btx123</a>.
-	   
+			<ul>
+				<li>Noel V, Ruscone M, Stoll G, Viara E, Zinovyev A, Barillot E, Calzone L. WebMaBoSS: A web interface for simulating Boolean models stochastically. <i>In Preparation</i>.</li>
+				<br/>
+				<li>Stoll G, Viara E, Barillot E, Calzone L. Continuous time Boolean modeling for biological signaling: application of Gillespie algorithm. BMC Syst Biol. 2012 Aug 29;6:116. doi: <a href="https://doi.org/10.1186/1752-0509-6-116">10.1186/1752-0509-6-116</a>.</li>
+				<br/>
+				<li>Stoll G, Caron B, Viara E, Dugourd A, Zinovyev A, Naldi A, Kroemer G, Barillot E, Calzone L.  MaBoSS 2.0: an environment for stochastic Boolean modeling. Bioinformatics btx123. 2017 Mar. DOI: <a href="https://doi.org/10.1093/bioinformatics/btx123">10.1093/bioinformatics/btx123</a>.</li>
+			</ul>
 	  		
 		
 		</FullPage>;
