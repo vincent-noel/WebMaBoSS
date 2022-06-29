a0fcec0ee9978ee596251735c86f4f031420a50b
Vincent Noël <contact@vincent-noel.fr>
2022-06-29 16:34:41 +0200

    Simplified frontend's url in django

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,17 +2,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-
-	path('', views.index, name="home"),
-	path('login/', views.index, name="login"),
-	path('logout/', views.index, name="logout"),
-	path('profile/', views.index, name="profile"),
-	path('register/', views.index, name="register"),
-	path('models/', views.index, name="models"),
-	path('data/', views.index, name="data"),
-	re_path('model/.*', views.index, name="model"),
-	re_path('profile/.*', views.index, name="model"),
-	re_path('tutorials/.*', views.index, name="model"),
-	re_path('about/.*', views.index, name="model"),
-	# re_path('images/.*', views.index, name="images"),
+	re_path(r'.*', views.index, name="home"),
 ]
