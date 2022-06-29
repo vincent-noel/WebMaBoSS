26d5fd12a5da50cbc90ccdb18343c938ececbe6c
Vincent Noël <contact@vincent-noel.fr>
2022-06-29 16:31:59 +0200

    Restoring minimisations of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,12 @@ module.exports = {
 	  }
 	]
   },
-//    optimization: {
-//     minimize: true,
+   optimization: {
+    minimize: true,
 //     minimizer: [new TerserPlugin()],
 //   	splitChunks: {
 //       chunks: 'all',
-// 	},
+	},
 //   },
 //   plugins: [
 //     new CompressionPlugin({
